06115d448c0af09f42d4c128b3169e35778c3499
Ole Rößler <ole.nelson.r@gmail.com>
2024-12-03 21:10:13 +0100

    preparing vercel

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel, Field
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from datetime import date
 from stock.StockManager import StockManager
+from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
 
 """
 Stock API
@@ -38,6 +41,18 @@ Example Usage:
 """
 
 app = FastAPI()
+
+# Add CORS middleware to allow cross-origin requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",  # Vite development server
+    ],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+
 stock_manager = StockManager()
 
 
@@ -48,63 +63,85 @@ class StockRequest(BaseModel):
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Welcome to the Stock API"}
 
 
 @app.get("/stocks/")
-def get_all_stocks():
+async def get_all_stocks():
     try:
         tickers = stock_manager.get_all_tickers()
     except ValueError as e:
-        return HTTPException(status_code=400, detail=str(e))
-    return {"tickers": tickers}
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+    return JSONResponse(content={"tickers": tickers}, status_code=200)
 
 
 @app.get("/stocks/{ticker}")
-def get_stock(ticker: str):
+async def get_stock(ticker: str):
     try:
         stock_json = stock_manager.get_stock_json(ticker)
     except ValueError as e:
-        return HTTPException(status_code=404, detail=str(e))
-    return stock_json
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+    return JSONResponse(content=stock_json, status_code=200)
 
 
 @app.put("/stocks/{ticker}")
-def update_stock(ticker: str):
+async def update_stock(ticker: str):
     try:
         stock_json = stock_manager.update_stock(ticker)
     except ValueError as e:
-        return HTTPException(status_code=400, detail=str(e))
-    return stock_json
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+    return JSONResponse(content=stock_json, status_code=200)
 
 
 @app.post("/stocks/")
-def add_stock(stock: StockRequest):
+async def add_stock(stock: StockRequest):
     try:
         start_date = stock.start_date.strftime("%Y-%m-%d")
         end_date = stock.end_date.strftime("%Y-%m-%d")
         stock_manager.add_stock(stock.ticker, start_date, end_date)
     except ValueError as e:
-        return HTTPException(status_code=400, detail=str(e))
-    return stock_manager.get_stock_json(stock.ticker)
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
+    return JSONResponse(
+        content=stock_manager.get_stock_json(stock.ticker), status_code=201
+    )
 
 
 @app.delete("/stocks/{ticker}")
-def remove_stock(ticker: str):
+async def remove_stock(ticker: str):
     try:
         stock_manager.delete_stock(ticker)
     except ValueError as e:
-        return HTTPException(status_code=400, detail=str(e))
-    return {"message": "Stock deleted", "ticker": ticker}
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
+    return JSONResponse(
+        content={"message": "Stock deleted", "ticker": ticker}, status_code=200
+    )
 
+
+# ASGI-Adapter für Serverless-Umgebungen
+handler = Mangum(app)
 
 """
 @app.get("/stocks/{ticker}/timeframe")
-def get_stock_timeframe(ticker: str, days: int):
+async def get_stock_timeframe(ticker: str, days: int):
     try:
         stock_json = stock_manager.get_stock_timeframe(ticker, days)
     except ValueError as e:
-        return HTTPException(status_code=404, detail=str(e))
-    return stock_json
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
+    return JSONResponse(content=stock_json, status_code=200)
 """
